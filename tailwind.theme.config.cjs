be39dbfca87d0d82da0511e9770680873f40d033
Nitin Singh <23051685@kiit.ac.in>
2026-07-15 00:42:19 +0530

    Update Tailwind theme configuration with new colors
    Refactor Tailwind theme configuration to use Tailwind's color palette and add a new 'purpleheart' theme.

--- tailwind.theme.config.cjs
+++ tailwind.theme.config.cjs
@@ -1,17 +1,38 @@
-default: {
-    colors: {
-        primary: '#1a1a1a',
-        secondary: '#000000',
-        dark: {
-            primary: '#1a1a1a',
-            secondary: '#000000'
-        },
-        accent: {
-            gray: {
-                light: colors.gray[300],
-                dark: colors.gray[600]
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purpleheart: {
+        colors: {
+            primary: colors.purple[700],
+            secondary: colors.purple[800],
+            dark: {
+                primary: colors.purple[300],
+                secondary: colors.purple[500]
             },
-            default: '#1a1a1a'
+            accent: {
+                gray: {
+                    light: colors.gray[300],
+                    dark: colors.gray[500]
+                },
+                default: colors.blue[700]
+            }
+        }
+    },
+    default: {
+        colors: {
+            primary: '#1a1a1a',
+            secondary: '#000000',
+            dark: {
+                primary: '#1a1a1a',
+                secondary: '#000000'
+            },
+            accent: {
+                gray: {
+                    light: colors.gray[300],
+                    dark: colors.gray[600]
+                },
+                default: '#1a1a1a'
+            }
         }
     }
 }
