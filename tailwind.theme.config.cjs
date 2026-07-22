f9c56c13ec0573db404e640e2a642a1567f6e4dc
Nitin Singh <23051685@kiit.ac.in>
2026-07-22 12:32:52 +0530

    Refactor theme configuration and update colors
    Removed the 'purpleheart' theme and updated the 'default' theme colors.

--- tailwind.theme.config.cjs
+++ tailwind.theme.config.cjs
@@ -1,37 +1,20 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purpleheart: {
-        colors: {
-            primary: colors.purple[700],
-            secondary: colors.purple[800],
-            dark: {
-                primary: colors.purple[300],
-                secondary: colors.purple[500]
-            },
-            accent: {
-                gray: {
-                    light: colors.gray[300],
-                    dark: colors.gray[500]
-                },
-                default: colors.blue[700]
-            }
-        }
-    },
     default: {
         colors: {
-            primary: '#334155',
-            secondary: '#1e293b',
+            primary: '#e05252',
+            secondary: '#c0392b',
             dark: {
-                primary: '#334155',
-                secondary: '#1e293b'
+                primary: '#e05252',
+                secondary: '#c0392b'
             },
             accent: {
                 gray: {
-                    light: colors.gray[300],
-                    dark: colors.gray[600]
+                    light: colors.gray[600],
+                    dark: colors.gray[400]
                 },
-                default: '#334155'
+                default: '#e05252'
             }
         }
     }
